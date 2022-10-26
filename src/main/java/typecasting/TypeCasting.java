f069eb56406ebb60612563e7ab75520b3b7d0326
nneslihanaydinn <nneslihanaydinn@gmail.com>
2022-10-27 01:18:48 +0300

    Type Casting

---
+++ src/main/java/typecasting/TypeCasting.java
@@ -0,0 +1,47 @@
+package typecasting;
+
+public class TypeCasting {
+
+        public static void main(String[] args) {
+
+            // TYPE CASTING
+            // SORU 1: Short data tipinde bir degisken olusturunuz ve onu bir int degiskenine donusturunuz.
+
+//        short yas = 12;
+//        int yeniYas = yas;
+
+            // SORU 2: Long data tipinde bir degisken olusturunuz ve onu bir int degiskenine donusturunuz.
+
+//        long yas = 12;
+//        int yeniYas = (int)yas;
+
+            //SORU 3: Double data tipinde bir degisken olusturunuz ve onu bir float degiskenine donusturunuz.
+
+//        double yas = 2.34;
+//        float yeniYas =(float)yas;
+
+//        SORU 4: Double data tipinde bir degisken olusturunuz ve onu bir short degiskenine donusturunuz. Sonrasinda bu
+//        short degiskenin degerini konsolda yazdiriniz.
+//        NOT: Ciktiya dikkat ediniz, ondalik bir deger olmayacaktir.
+
+//        double fiyat = 13.9;
+//        short yeniFiyat = (short)fiyat;
+//        System.out.println(yeniFiyat);
+
+//        SORU 5: Byte data tipinde bir degisken olusturunuz ve onu bir double degiskenine donusturunuz. Sonrasinda
+//        bu short degiskenin degerini konsolda yazdiriniz.
+//        NOT: Ciktiya dikkat ediniz, ondalik bir deger olmayacaktir.
+//
+//        byte age = 7;
+//        double yeniAge = age;
+//        System.out.println(yeniAge);
+
+
+
+
+
+
+        }
+
+    }
+
